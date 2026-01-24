677348593026a9ace29feb6cd7fa6ce5b1ce2494
humphreyn_risk <humphreyn@b2b.regn.net>
2026-01-24 02:49:04 +0000

    build(dependencies): bump
    fix commit/push

--- functions/engines/GraphicsMagick.js
+++ functions/engines/GraphicsMagick.js
@@ -25,7 +25,7 @@ class GraphicsMagick {
 				const pngFile = path.resolve(pngFileObj.dir, pngFileObj.name + pngExtension);
 
 				if (config.settings.password) {
-					gm(pdfBuffer, pdfFilename)
+					this.gm(pdfBuffer, pdfFilename)
 						.command("convert")
 						.in("-authenticate", config.settings.password)
 						.out(multiPage ? "+adjoin" : "-adjoin")
@@ -36,7 +36,7 @@ class GraphicsMagick {
 							return resolve();
 						});
 				} else {
-					gm(pdfBuffer, pdfFilename)
+					this.gm(pdfBuffer, pdfFilename)
 						.command("convert")
 						.out(multiPage ? "+adjoin" : "-adjoin")
 						.density(config.settings.density, config.settings.density)
@@ -52,7 +52,7 @@ class GraphicsMagick {
 
 	applyMask(pngFilePath, coordinates = { x0: 0, y0: 0, x1: 0, y1: 0 }, color = "black") {
 		return new Promise((resolve, reject) => {
-			gm(pngFilePath)
+			this.gm(pngFilePath)
 				.command("convert")
 				.drawRectangle(coordinates.x0, coordinates.y0, coordinates.x1, coordinates.y1)
 				.fill(color)
@@ -64,7 +64,7 @@ class GraphicsMagick {
 
 	applyCrop(pngFilePath, coordinates = { width: 0, height: 0, x: 0, y: 0 }, index = 0) {
 		return new Promise((resolve, reject) => {
-			gm(pngFilePath)
+			this.gm(pngFilePath)
 				.command("convert")
 				.crop(coordinates.width, coordinates.height, coordinates.x, coordinates.y)
 				.write(pngFilePath.replace(".png", `-${index}.png`), (err) => {
@@ -75,78 +75,3 @@ class GraphicsMagick {
 }
 
 export default GraphicsMagick;
-
-// /**
-//  *
-//  * @param {('graphicsMagick'|'imageMagick')} [engine=graphicsMagick]  - optional engine, Default is 'graphicsMagick'
-//  * @returns {{}}
-//  */
-// export default function (engine = "graphicsMagick") {
-// 	const gm = engine === "imageMagick" ? gm.subClass({ imageMagick: "7+" }) : gm;
-//
-// 	const module = {};
-//
-// 	module.pdfToPng = (pdfDetails, pngFilePath, config) => {
-// 		const options = pdfDetails.buffer ? {data: new Uint8Array(pdfDetails.buffer)} : {url: pdfDetails.filename};
-// 		if (config.settings.hasOwnProperty('password')) options.password = config.settings.password;
-// 		const loadingTask = pdfjsLib.getDocument(options);
-// 		return loadingTask.promise.then((document) => {
-// 			return new Promise((resolve, reject) => {
-// 				const pdfBuffer = pdfDetails.buffer;
-// 				const pdfFilename = path.parse(pdfDetails.filename).base;
-// 				const pngFileObj = path.parse(pngFilePath);
-// 				const multiPage = document.numPages > 1;
-// 				const pngExtension = multiPage ? "-%d.png" : ".png"
-// 				const pngFile = path.resolve(pngFileObj.dir, pngFileObj.name + pngExtension);
-//
-// 				if (config.settings.hasOwnProperty('password')) {
-// 					gm(pdfBuffer, pdfFilename)
-// 						.command('convert')
-// 						.in('-authenticate', config.settings.password)
-// 						.out(multiPage ? "+adjoin" : "-adjoin")
-// 						.density(config.settings.density, config.settings.density)
-// 						.quality(config.settings.quality)
-// 						.write(pngFile, (err) => {
-// 							if (err) return reject(err);
-// 							return resolve();
-// 						});
-// 				} else {
-// 					gm(pdfBuffer, pdfFilename)
-// 						.command('convert')
-// 						.out(multiPage ? "+adjoin" : "-adjoin")
-// 						.density(config.settings.density, config.settings.density)
-// 						.quality(config.settings.quality)
-// 						.write(pngFile, (err) => {
-// 							if (err) return reject(err);
-// 							return resolve();
-// 						});
-// 				}
-// 			});
-// 		});
-// 	};
-//
-// 	module.applyMask = (pngFilePath, coordinates = { x0: 0, y0: 0, x1: 0, y1: 0 }, color = 'black') => {
-// 		return new Promise((resolve, reject) => {
-// 			gm(pngFilePath)
-// 				.command('convert')
-// 				.drawRectangle(coordinates.x0, coordinates.y0, coordinates.x1, coordinates.y1)
-// 				.fill(color)
-// 				.write(pngFilePath, (err) => {
-// 					err ? reject(err) : resolve();
-// 				});
-// 		});
-// 	};
-//
-// 	module.applyCrop = (pngFilePath, coordinates = { width: 0, height: 0, x: 0, y: 0 }, index = 0) => {
-// 		return new Promise((resolve, reject) => {
-// 			gm(pngFilePath)
-// 				.command('convert')
-// 				.crop(coordinates.width, coordinates.height, coordinates.x, coordinates.y)
-// 				.write(pngFilePath.replace('.png', `-${index}.png`), (err) => {
-// 					err ? reject(err) : resolve();
-// 				});
-// 		});
-// 	};
-//
-// 	return module;
-// };
